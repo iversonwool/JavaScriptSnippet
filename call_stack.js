37f9169ade6e7c8a2294ea6214de1f234a7cc887
李浩 <286300785@qq.com>
2018-10-24 00:30:21 +0800

    task, microtask

--- call_stack.js
+++ call_stack.js
@@ -23,3 +23,20 @@ console.log('script end')
 
 // tasks and microtasks
 
+//asynchronous
+
+function asyncForEach(array, cb) {
+  // setTimeout()
+  array.forEach(function (i) {
+    // delay()
+    setTimeout(() => cb(i), 0)
+  })
+}
+
+asyncForEach([1, 2, 3, 4, 5], (i) => {
+  console.log(i)
+});
+
+[1, 2, 3].forEach((i) => {
+  console.log(i)
+})
