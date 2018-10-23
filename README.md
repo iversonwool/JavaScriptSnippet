2129a982c189b1845ce0dbe5e652ea6a1edb1b84
LeeHow <iversonwool@sina.com>
2018-10-23 17:18:24 +0800

    call stack

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # JavaScriptSnippet
 Some snippets about JavaScript.
+
+
+
+## Call stack
+
+
+* [Tasks, microtasks, queues and schedules](https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/)
---
+++ call_stack.js
@@ -0,0 +1,25 @@
+console.log('script start')
+
+setTimeout(function () {
+  console.log('setTimeout')
+}, 0)
+
+Promise.resolve()
+  .then(function () {
+    console.log('promise1')
+  })
+  .then(function () {
+    console.log('promise2')
+  })
+
+console.log('script end')
+
+//order
+// script start
+// script end
+// promise1
+// promise2
+// setTimeout
+
+// tasks and microtasks
+
