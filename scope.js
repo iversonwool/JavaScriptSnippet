77da89674449d9753ab3a6cca9b9e9e1815bc1dc
LeeHow <iversonwool@126.com>
2021-03-24 15:24:52 +0800

    作用域 问题

---
+++ scope.js
@@ -0,0 +1,15 @@
+name = 2
+// console.log(name)
+
+
+var name1 = 'sss'
+
+//
+// function func() {
+//   var1 = 'sss'
+// }
+//
+// console.log(this.var1)
+
+console.log(JSON.stringify(null))
+console.log(undefined)
