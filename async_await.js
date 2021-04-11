85a3427ffa1fdfc03ade3f24c05e8b1a229ad34d
李浩 <iversonwool@sina.com>
2021-04-11 22:01:38 +0800

    async await function

--- async_await.js
+++ async_await.js
@@ -17,3 +17,21 @@ const add = async function (x) {
 add(10).then(v => {
   console.log(v)
 })
+
+// [resolveAfter2Seconds(90)]
+
+
+// []
+async function func1(a) {
+
+  console.log('async function without return value')
+  // return a
+
+}
+
+
+async function func2() {
+  const value = await func1('hello world!')
+  console.log('value is ', value)
+}
+func2()
