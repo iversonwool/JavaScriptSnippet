92b831a67697f479bc58caa785910a183452a052
Lee_Klaus <iversonwoool@gmail.com>
2022-11-22 11:22:05 +0800

    feat: tree 查找直接子节点 和 所有子节点
    tree 数据常规操作

--- tree.js
+++ tree.js
@@ -63,11 +63,188 @@ const getParentKey = (key, tree) => {
 
 
 // value 是搜索关键字
-const expandedKeys = dataList
-  .map(item => {
-    if (item.title.indexOf(value) > -1) {
-      return getParentKey(item.key, gData);
-    }
-    return null;
+// const expandedKeys = dataList
+//   .map(item => {
+//     if (item.title.indexOf(value) > -1) {
+//       return getParentKey(item.key, gData);
+//     }
+//     return null;
+//   })
+//   .filter((item, i, self) => item && self.indexOf(item) === i);
+
+
+
+
+
+let cId = 4;
+let tree = [
+  {
+    id: 0,
+    children: [
+      {
+        id: 1,
+        children: [
+          {
+            id: 3
+          }
+        ]
+      },
+      {
+        id: 2,
+        children: [
+          {
+            id: 4,
+            children: [
+              {
+                id: 5,
+                children: [
+                  {
+                    id: 6
+                  }
+                ]
+              },
+              {
+                id: 20
+              },
+              {
+                id: 21
+              }
+            ]
+          },
+          {
+            id: 7,
+            children: [
+              {
+                id: 8
+              }
+            ]
+          }
+        ]
+      },
+      {
+        id: 9,
+        children: [
+          {
+            id: 10,
+            children: [
+              {
+                id: 11
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
+let res = []
+// false 代表只找不查  true 代表找也查
+function findChildren(item, flag) {
+  // 判断是否找到那个节点，如果找到则把flag改为true
+  if (item.id === cId) {
+    flag = true
+  }
+  // 如果flag为true，那么就存入子数组
+  if (flag) {
+    res.push(item.id)
+  }
+  if (!item.children) {
+    return;
+  }
+  item.children.forEach(function (child) {
+    findChildren(child, flag);
   })
-  .filter((item, i, self) => item && self.indexOf(item) === i);
+}
+tree.forEach(function (item) {
+  findChildren(item, false)
+});
+console.log(res);
+
+
+function getBrotherNodes(list, id) {
+  for(let i in list) {
+    if(list[i].id === id) {
+      return list.filter(v => v.id !== id)
+    }
+    if(list[i].children?.length > 0) {
+      let node = getBrotherNodes(list[i].children, id)
+      if(node) return node.filter(v => v.id !== id)
+    }
+  }
+}
+
+const sibling = getBrotherNodes(tree, 4)
+console.log('sibling', sibling)
+
+
+
+
+function getAllChildrenNodes(list, id, arr = []) {
+  for(let i in list) {
+    if(list[i].id === id) {
+      arr.push(list[i])
+      if(list[i].children?.length > 0) {
+        getChild(list[i].children, arr)
+      }
+    }else {
+      if(list[i].children?.length > 0) {
+        getAllChildrenNodes(list[i].children, id, arr)
+      }
+    }
+  }
+  return arr.filter(v => v.id !== id)
+}
+
+function getChild(list,  arr) {
+  list.forEach(v => {
+    arr.push(v)
+    if(v.children) {
+      getChild(v.children, arr)
+    }
+  })
+}
+
+
+function getChildrenNodes(list, id, arr = []) {
+  for(let i in list) {
+    if(list[i].id === id) {
+      arr.push(list[i])
+      if(list[i].children?.length > 0) {
+        getDirectChildren(list[i].children, arr)
+      }
+    }else {
+      if(list[i].children?.length > 0) {
+        getChildrenNodes(list[i].children, id, arr)
+      }
+    }
+  }
+  console.log('arr', arr)
+  return arr.filter(v => v.id !== id)
+}
+
+function getDirectChildren(list, arr) {
+  list.forEach(v => {
+    arr.push(v)
+    // if(v.children) {
+    //   getChild(v.children, arr)
+    // }
+  })
+}
+
+console.log('-', getChildrenNodes(tree, 4))
+
+
+function findDirectChildren(tree = [], id, arr = []) {
+  for (const treeElement of tree) {
+    if (treeElement.id === id) {
+      arr.push(...treeElement.children)
+    } else {
+      findDirectChildren(treeElement.children, id, arr)
+    }
+  }
+
+  return arr
+}
+
+const directR = findDirectChildren(tree, 4)
+console.log('directR', directR)
