435e558a30127c92795bc3d531034e46b4320b24
Lee_Klaus <iversonwoool@gmail.com>
2024-07-25 10:24:23 +0800

    异步 generator

--- generator.js
+++ generator.js
@@ -50,3 +50,24 @@ const gtr = pseudoRandom(1)
 console.log(gtr.next().value)
 console.log(gtr.next().value)
 console.log(gtr.next().value)
+
+
+
+let range = {
+  from: 1,
+  to: 5,
+  async *[Symbol.asyncIterator]() {
+    for (let value = this.from; value <= this.to; value++) {
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      yield value
+    }
+  }
+}
+
+async function getValue() {
+  for await (let value of range) {
+    console.log('value', value)
+  }
+}
+
+getValue();
